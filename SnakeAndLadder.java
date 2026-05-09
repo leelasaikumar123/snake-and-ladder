ae521f870596ae3946f6ea9f4f45f2484bfd39f7
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-09 10:28:29 +0530

    feat:added a dice that gives us a random number b/w 1-6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,10 @@
+import java.util.Random;
+
 public class SnakeAndLadder{
     public static void main(String[] args) {
-       
+      Random random=new Random(); 
      System.out.println("Welcome to Snake And Ladder Simulator");
        int first_player_score=0; 
+       int dice=random.nextInt(6)+1;
     }
 }
