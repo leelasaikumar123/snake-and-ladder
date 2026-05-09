c1bc67ce7431e5719630d407f2c0375099e7f94d
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-09 10:25:44 +0530

    feat:added a single player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,7 @@
 public class SnakeAndLadder{
     public static void main(String[] args) {
        
-     System.out.println("Welcome to Snake And Ladder Simulator");   
+     System.out.println("Welcome to Snake And Ladder Simulator");
+       int first_player_score=0; 
     }
 }
